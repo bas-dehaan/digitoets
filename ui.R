54b663bf63aa94899a6a97e6b6758ae1d6e71d90
bas-dehaan <9017270+bas-dehaan@users.noreply.github.com>
2020-03-09 16:00:15 +0100

    First commit

---
+++ ui.R
@@ -0,0 +1,47 @@
+#
+# Created by: B.M. de Haan
+# Used to analyse data from a school servey about digital examination
+# Github: https://github.com/bas-dehaan/digitoets
+#
+
+library(shiny)
+
+# Define UI for application
+shinyUI(fluidPage(
+
+    # Application title
+    titlePanel("Uitslag enquête digitaal tentamen"),
+
+    # Sidebar with the input for the data selection
+    sidebarLayout(
+        sidebarPanel(
+            # Select course program
+            checkboxGroupInput("opleiding", 
+                h4("Opleiding"), 
+                choices = list("BML-R"  = 1, 
+                               "BML-MD" = 2, 
+                               "C"      = 3,
+                               "CT"     = 4,
+                               "BIN"    = 5,
+                               "DSLS"   = 6
+                               ),
+                               selected = c(1,2,3,4,5,6)
+            ),
+            # Select course year
+            checkboxGroupInput("jaar", 
+                h4("Leerjaar"), 
+                choices = list("Jaar 1" = 1, 
+                               "Jaar 2" = 2, 
+                               "Jaar 3" = 3,
+                               "Jaar 4" = 4
+                               ),
+                               selected = c(1,2,3,4)
+            )
+        ),
+
+        # Show the statistics of the selected data
+        mainPanel(
+            #output
+        )
+    )
+))
